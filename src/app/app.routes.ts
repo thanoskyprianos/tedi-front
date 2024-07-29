913a7abc03e8473bb9eed12462e8b8efebca3fe3
damianosp <deetechdum@gmail.com>
2024-07-29 19:18:11 +0300

    Sign In

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {SignInComponent} from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent, title: 'Sign In'}
+];
+
+export default routes;
