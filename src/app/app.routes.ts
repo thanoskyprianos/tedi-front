e0c4e11c560e25b56f7d41cfa2ffc9dd3a1fb27d
damianosp <deetechdum@gmail.com>
2024-07-30 20:08:26 +0300

    Welcome page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,18 @@ import {SignInComponent} from './sign-in/sign-in.component';
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent, title: 'NetWork' },
   { path: 'sign-in', component: SignInComponent, title: 'Sign In'},
   { path: 'sign-up', component: SignUpComponent, title: 'Sign Up'},
   { path: 'home-page', component: HomePageComponent, title: 'Home Page' },
+  { path: 'profile', component: ProfileComponent, title: 'Profile'}
 ];
 
 export default routes;
+
+/* Όταν συνδέσουμε back-front θα πρέπει 
+να δημιουργηθούν ξεχωριστά URL βάσει του id
+οπότε αλλαγή των routes */
