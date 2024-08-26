f67c8fbdf567a7f76aebaea63f4af88bd40ac9a0
damianosp <deetechdum@gmail.com>
2024-08-27 01:45:06 +0300

    Network: Show aboutMe, picture and navigate to profile

--- src/app/components/network/network.component.ts
+++ src/app/components/network/network.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
-import {UserModule} from "../../modules/user.module";
+import {Component, Input} from '@angular/core';
+import {ProfileData, UserModule} from "../../modules/user.module";
 import {UserSessionService} from "../../services/user-session.service";
 import {UserFetcherService} from "../../services/user-fetcher.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {NavigationExtras, Router} from "@angular/router";
 
 @Component({
   selector: 'app-network',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './network.component.html',
   styleUrl: './network.component.css'
 })
+
 export class NetworkComponent {
   user!: UserModule;
   displayUser!: UserModule;
   connectedWithMe: any[] = [];
   avatarUrl: string = '';
+  avatarUrls: string[] = [];
+  aboutMeData: { [userId: number]: ProfileData } = {};
+
+  aboutMe: ProfileData = new ProfileData (
+    null,
+    null,
+    null,
+    null,
+    null
+  )
+
+  @Input() selectedFile: File | null = null;
 
   constructor(
     protected session: UserSessionService,
@@ -51,6 +65,10 @@ export class NetworkComponent {
 
   connectionsSet(info: any) {
     this.connectedWithMe = info._embedded.userList as UserModule[];
+    this.connectedWithMe.forEach(user => {
+      this.getAboutMe(user);
+      this.setAvatar(user, user.id);
+    });
   }
 
   goToUser(event: any, userId: number) {
@@ -63,11 +81,39 @@ export class NetworkComponent {
     this.router.navigate(['/profile-page', userId], selectedUser);
   }
 
-  setAvatar() {
-    this.fetcher.avatar(this.user!._links.avatar.href).subscribe({
+  getAboutMe(user: UserModule) {
+    const infoUrl = user._links.info;
+    if (infoUrl) {
+      this.fetcher.aboutMe(infoUrl.href).subscribe(
+        (res: any) => {
+          if (res.status === 200) {
+            console.log('About Me data:', res.body);
+            this.aboutMeSet(user.id, res.body);
+          }
+        }
+      )
+    }
+  }
+
+  aboutMeSet(userId: number, info: any) {
+    this.aboutMeData[userId] = new ProfileData(
+      info.professionalPosition,
+      info.employmentAgency,
+      info.experience,
+      info.education,
+      info.skills
+    );
+  }
+
+  setAvatar(user: UserModule, userId: number) {
+    const avatarUrl = user._links.avatar?.href;
+    this.fetcher.avatar(avatarUrl).subscribe({
       next: (res) => {
         if (!res.body) return;
-        this.avatarUrl = URL.createObjectURL(res.body);
+
+        const newAvatarUrl = URL.createObjectURL(res.body);
+        this.avatarUrls[userId] = newAvatarUrl;
+        console.log('Avatar obtained:', newAvatarUrl);
       }
     })
   }
